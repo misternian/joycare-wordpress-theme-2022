35cf95d1d8747d0967c0be1db110d9a43909b5f9
Nian <misternian@outlook.com>
2022-06-15 15:57:30 +0800

    Init

---
+++ footer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package tailwindcss
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="" class="">
+		<div class="">
+			<p>联系我们：</p>
+			<p>江苏省扬州市维扬路283号</p>
+			<p>13952746510 (0514)87870515</p>
+			<p>jacksonfei@vip.163.com</p>
+			<p>jackson_fei</p>
+		</div>
+		<div class="text-center py-6">
+			Copyright © 2022 扬州洁彩口腔护理用品有限公司. 苏ICP备14052651号-1
+		</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package tailwindcss
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class('leading-normal'); ?>>
+
+<div class="bg-navy py-2 px-6">
+	<div class="text-sm bg-transparent flex text-white">
+		<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  		<path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+ 		 	<path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+		</svg>
+		<div class="ml-1">jacksonfei@vip.163.com</div>
+		<svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  		<path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+		</svg>
+		<div class="ml-1">+86 13952746510</div>
+	 
+	</div>
+</div>
+
+<div id="page" class="site">
+	
+
+	<header id="masthead" class="site-header container mx-auto flex">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class=""><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$tailwindcss_description = get_bloginfo( 'description', 'display' );
+			if ( $tailwindcss_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $tailwindcss_description; /* WPCS: xss ok. */ ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation mx-0 my-0">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+				<span class="inline-block w-4 h-4">
+					<!-- menu button icon courtesy of FontAwesome -->
+					<!-- license informationL https://fontawesome.com/license -->
+					<!-- icon modified to include title tag -->
+					<svg aria-hidden="true" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><title>three horizontal bars</title><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
+				</span>
+				<?php esc_html_e( 'Primary Menu', 'tailwindcss' ); ?>
+			</button>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			) );
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
